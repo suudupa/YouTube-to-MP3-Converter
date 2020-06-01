4a0b34f7e93b92f5c8573b753fb3e65e83e83325
suudupa <suhas.udupa@mail.mcgill.ca>
2020-06-01 13:19:44 -0400

    download progress needs to be fixed

--- src/main/java/com/suudupa/youtubetomp3/Main.java
+++ src/main/java/com/suudupa/youtubetomp3/Main.java
@@ -1,6 +1,5 @@
 package com.suudupa.youtubetomp3;
 
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -9,38 +8,37 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) throws IOException, InterruptedException {
 
-        //get video URL
         System.out.println("Welcome to the YouTube to MP3 Java converter!\n");
+
+
+        //get converter URL
         System.out.print("Enter the URL of the Youtube video: ");
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String url = reader.readLine().trim();
-        String key = null;
+        String converter = null;
         try {
-            key = getKey(url);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Sorry, there was an error converting the given URL (invalid format)!\n");
-            exit(1);
+            converter = Utils.convertUrl(url);
+        } catch (NullPointerException e) {
+            System.out.println("Error parsing the given URL (invalid format)!\n");
+            Utils.exit(1);
         }
 
 
-        //convert to URL object
-        URL converter = null;
+        //get download URL
+        String downloader = null;
         try {
-            converter = new URL("https://mp3fy.com/yfy/" + key);
-        } catch (MalformedURLException e) {
-            System.out.println("Sorry, there was an error converting the given URL (URL doesn't exist)!\n");
-            exit(1);
+            downloader = Utils.getDownloadUrl(converter);
+            System.out.println(url + " successfully parsed and converted.\n");
+        } catch (IOException e) {
+            System.out.println("Error fetching the download link!\n");
+            Utils.exit(2);
         }
-
-        System.out.println(url + " successfully parsed.\n");
 
 
         //get download path
@@ -49,7 +47,10 @@ public class Main {
         String path = reader.readLine().trim();
         File dir = new File(path);
         if (dir.exists() && dir.isDirectory()) System.out.println("Found " + path + "\n");
-        else exit(2);
+        else {
+            System.out.print("Could not locate " + path + "\n");
+            Utils.exit(3);
+        }
 
 
         //get current project directory
@@ -59,81 +60,16 @@ public class Main {
 
         //start chromedriver.exe
         ChromeOptions chromeOptions = new ChromeOptions();
-        //chromeOptions.addArguments("--headless --disable-gpu");
-        chromeOptions.addArguments("--headless");
+        chromeOptions.addArguments("--headless --disable-gpu");
         HashMap<String, Object> chromePreferences = new HashMap<>();
         chromePreferences.put("safebrowsing.enabled", false);
         chromePreferences.put("profile.default_content_settings.popups", 0);
         chromePreferences.put("download.default_directory", path);
         chromeOptions.setExperimentalOption("prefs", chromePreferences);
         WebDriver chromeDriver = new ChromeDriver(chromeOptions);
-        chromeDriver.get(String.valueOf(converter));
-        getDownloadProgress(chromeDriver);
+        chromeDriver.get(downloader);
+        Utils.getDownloadProgress(chromeDriver);
         chromeDriver.close();
-    }
-
-
-    //get key associated to the YouTube video
-    private static String getKey(String url) throws ArrayIndexOutOfBoundsException {
-        if (url.contains("=")) {
-            String key = url.split("=")[1];
-            if (key.contains("&")) {
-                return key.split("&")[0];
-            } else {
-                return key;
-            }
-        } else {
-            return url.split("/")[3];
-        }
-    }
-
-
-    //get download progress of MP3 file
-    private static void getDownloadProgress(WebDriver driver) throws InterruptedException {
-
-        //open chrome downloads tab
-        String mainWindow = driver.getWindowHandle();
-        JavascriptExecutor newTab = (JavascriptExecutor) driver;
-        newTab.executeScript("window.open()");
-        for (String winHandle : driver.getWindowHandles()) {
-            driver.switchTo().window(winHandle);
-        }
-        driver.get("chrome://downloads");
-
-        //parse downloads tab to get download progress
-        JavascriptExecutor downloads = (JavascriptExecutor) driver;
-        boolean isFirstIteration = true;
-        boolean isDownloadStarting = false;
-        Long prevPercentage = (long) -1;
-        Long percentage = (long) 0;
-        long downloadStart = System.currentTimeMillis();
-        while (percentage != 100) {
-            try {
-                percentage = (Long) downloads.executeScript("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('#progress').value");
-                if (isDownloadStarting) System.out.println("Starting download...");
-                if (!prevPercentage.equals(percentage)) System.out.println(percentage + "% completed");
-                prevPercentage = percentage;
-                isDownloadStarting = false;
-            } catch (Exception e) {
-                if (isFirstIteration) System.out.println("\nWaiting for server to begin download...");
-            }
-            Thread.sleep(100);
-            isFirstIteration = false;
-        }
-
-        //download completed
-        long downloadEnd = System.currentTimeMillis();
-        String filename = (String) downloads.executeScript("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('div#content #file-link').text");
-        String downloadPath = (String) downloads.executeScript("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('div.is-active.focus-row-active #file-icon-wrapper img').src");
-        File file = new File(downloadPath);
-        long downloadTime = downloadEnd - downloadStart;
-        System.out.println("\n" + filename + " successfully downloaded in " + downloadPath + " (" + file.length() + " bytes" + " in " + downloadTime/1000L + "s.");
-        driver.close();
-        driver.switchTo().window(mainWindow);
-    }
-
-    private static void exit(int status) {
-        System.out.println("Exiting program...\n");
-        System.exit(status);
+        Utils.exit(0);
     }
 }
---
+++ src/main/java/com/suudupa/youtubetomp3/Utils.java
@@ -0,0 +1,74 @@
+package com.suudupa.youtubetomp3;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+import java.io.IOException;
+
+public class Utils {
+
+    private static final String DELIMITER = "youtube";
+    private static final String CONVERTER = "320";
+    private static final String DOWNLOAD_ELEMENT = "a.btn.btn-success.btn-lg";
+    private static final String DOWNLOAD_LINK = "abs:href";
+    private static final String NEW_TAB_SCRIPT = "window.open()";
+    private static final String DOWNLOADS_TAB = "chrome://downloads";
+
+    private static final int SLEEP = 100;
+
+    //converter URL
+    public static String convertUrl(String url) throws NullPointerException {
+        int index = url.indexOf(DELIMITER);
+        StringBuilder convertedUrl = new StringBuilder(url);
+        return convertedUrl.insert(index, CONVERTER).toString();
+    }
+
+    //download URL
+    public static String getDownloadUrl(String url) throws IOException {
+        Document pageSource = Jsoup.connect(url).get();
+        return pageSource.select(DOWNLOAD_ELEMENT).first().attr(DOWNLOAD_LINK);
+    }
+
+    //get download progress of MP3 file
+    public static void getDownloadProgress(WebDriver driver) throws InterruptedException {
+
+        //open chrome downloads tab
+        String mainWindow = driver.getWindowHandle();
+        JavascriptExecutor newTab = (JavascriptExecutor) driver;
+        newTab.executeScript(NEW_TAB_SCRIPT);
+        for (String winHandle : driver.getWindowHandles()) {
+            driver.switchTo().window(winHandle);
+        }
+        driver.get(DOWNLOADS_TAB);
+
+        //parse downloads tab to get download progress
+        JavascriptExecutor downloads = (JavascriptExecutor) driver;
+        Long percentage = (long) 0;
+        long downloadStart = System.currentTimeMillis();
+        while (percentage != 100) {
+            try {
+                percentage = (Long) downloads.executeScript("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('#progress').value");
+                System.out.println(percentage + "% completed");
+            } catch (Exception e) {
+                System.out.println("\nWaiting for server to begin download...");
+            }
+            //Thread.sleep(SLEEP);
+        }
+
+        //download completed
+        long downloadEnd = System.currentTimeMillis();
+        String filename = (String) downloads.executeScript("return document.querySelector('downloads-manager').shadowRoot.querySelector('#downloadsList downloads-item').shadowRoot.querySelector('div#content #file-link').text");
+        long downloadTime = (downloadEnd - downloadStart) / 1000L;
+        System.out.println("\n" + filename + " successfully downloaded in " + downloadTime + "s." + "\n");
+        driver.close();
+        driver.switchTo().window(mainWindow);
+    }
+
+    //exit program
+    public static void exit(int status) {
+        System.out.println("Exiting program...\n");
+        System.exit(status);
+    }
+}
